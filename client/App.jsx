ed5497425fd4e1c301d96eb0c03f553d1a82c669
Kamil Witkowski <53374963+KamWit7@users.noreply.github.com>
2022-09-14 19:43:41 +0200

    module 4 end

--- client/App.jsx
+++ client/App.jsx
@@ -4,10 +4,20 @@ const App = ({ questions, answers }) => {
   return (
     <div>
       <h1>Q&A Tool</h1>
-
       {questions.map(({ questionId, content }) => (
         <div key={questionId}>
           <h3>{content}</h3>
+          <div>
+            {answers
+              .filter((answers) => answers.questionId === questionId)
+              .map(({ content, upvotes, answersId }) => (
+                <div key={'x' + answersId}>
+                  <span>
+                    {content} - {upvotes}
+                  </span>
+                </div>
+              ))}
+          </div>
         </div>
       ))}
     </div>
